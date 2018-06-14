d2892edd37dd9783e569c65aa816a514146cd9af
AMSolovyev <alexander.solovyev7@gmail.com>
2018-06-14 19:08:17 +0300

    I have done the script which looks the midnighters

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Night Owls Detector
+#Seek midnigthers
 
-[TODO. There will be project description]
+The program ```seek_dev_nighters.py``` looks for people to put the script during night
 
-# Project Goals
+#How to use
+```
+virtualenv -p python3 env
+source env/bin/activate
+pip install -r requirements.txt
+python3 seek_dev_nighters.py
+```
+
+You can get data like this:
+```
+id311542249
+timurgosu
+ id311542249
+timurgosu
+ id311542249
+timurgosu
+ id311542249
+ oeananas
+timurgosu
+ id311542249
+ oeananas
+timurgosu
+ id311542249
+ oeananas
+```
+
+#Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,40 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+from pytz import timezone
+import requests
+from datetime import datetime, time
+import json
 
-def get_midnighters():
-    pass
+
+def load_attempts(pages=10):
+    url = 'http://devman.org/api/challenges/solution_attempts'
+    try:
+        for page in range(1, pages + 1):
+            params = {'page': page}
+            devman_data = requests.get(
+                url, params=params).json()
+            for record in devman_data['records']:
+                yield record
+
+    except json.decoder.JSONDecodeError:
+        return None
+
+
+def get_midnighters(record):
+    start_time = 0
+    finish_time = 6
+    time_midnighters = 0
+    if record['timestamp']:
+        local_time = datetime.fromtimestamp(
+            record['timestamp'],
+            timezone(record['timezone'])
+        )
+        time_midnighters = local_time.time()
+    if start_time < time_midnighters.hour < finish_time:
+        return True
+
 
 if __name__ == '__main__':
-  pass
+    midnighters = set()
+    for record in load_attempts():
+        if get_midnighters(record):
+            midnighters.add(record['username'])
+            print('\n '.join(midnighters))
